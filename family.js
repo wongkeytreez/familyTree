e8494beb35095f2145f484e4940ea846dcea8e81
tank juice <110167894+wongkeytreez@users.noreply.github.com>
2025-07-13 07:22:48 +0700

    Create family.js

---
+++ family.js
@@ -0,0 +1,36 @@
+function person(age, name, gender) {
+  this.age = age;
+  this.name = name;
+  this.children = [];
+  this.parent1 = null;
+  this.parent2 = null;
+  this.gender = gender; // true = male
+  this.spouse = null;
+}
+
+function marry(p1, p2) {
+  p1.spouse = p2;
+  p2.spouse = p1;
+}
+
+function divorce(p1, p2) {
+  p1.spouse = null;
+  p2.spouse = null;
+}
+
+function makeKid(name, gender, parent1, parent2) {
+  const kid = new person(0, name, gender);
+  kid.parent1 = parent1;
+  kid.parent2 = parent2;
+  parent1.children.push(kid);
+  parent2.children.push(kid);
+  return kid;
+}
+
+// Global people list
+let people = [];
+loadFromGitHub((loadedData) => {
+  people = loadedData;
+  draw(false);
+
+})
